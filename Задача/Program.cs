db130efff400a0d26b7158276e470329ac0ee38a
as0258@icloud.com <as0258@icloud.com>
2023-03-10 02:48:13 +0300

    start push

--- Задача/Program.cs
+++ Задача/Program.cs
@@ -1,42 +1,39 @@
-﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
-// длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
-// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
-// лучше обойтись исключительно массивами.
-// Примеры:
-// ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
-// ["1234", "1567", "-2", "computer science"] -> ["-2"]
-// ["Russia", "Denmark", "Kazan"] -> []
+﻿using System;
+using static System.Console;
 
-string[] array = { "hello", "2", "world", ":-)", "1234", "1567", "Rus",
-"-2", "computer science", "Russia", "Denmark", "Kazan", "GB", "GBU" };
-int count = ArrayCheck(array);
-string[] resultArray = FillResultArray(array, count);
-int ArrayCheck(string[] array)
-{
- int count = 0;
- for (int i = 0; i < array.Length; i++)
-    {
- if (array[i].Length <= 3) count++;
-    }
- return count;
-}
-string[] FillResultArray(string[] array, int count)
-{
- string[] result = new string[count];
- for (int i = 0; i < array.Length; i++)
-    {
- if (array[i].Length <= 3)
-        {
- result[result.Length - count] = array[i];
- count--;
+Clear();
+
+string[] array = AskArray();
+string[] result = FindLessThan(array, 3);
+WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] FindLessThan(string[] input, int n) {
+    string[] output = new string[CountLessThan(input, n)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            output[j] = input[i];
+            j++;
         }
     }
- return result;
+
+    return output;
 }
-void PrintArray(string[] array)
-{
- Console.Write("[");
- for (int i = 0; i < array.Length - 1; i++) Console.Write($"{array[i]}, ");
- Console.Write($"{array[array.Length - 1]}]");
+
+int CountLessThan(string[] input, int n) {
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            count++;
+        }
+    }
+
+    return count;
 }
-PrintArray(resultArray);
+
+string[] AskArray() {
+    Write("Введите значения через пробел: ");
+    return ReadLine()!.Split(" ");
+}
+
